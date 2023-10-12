367b11c35597ffc9689acab820788aa8bba6c402
TameNest37 <oliveirarenann81@gmail.com>
2023-10-12 16:32:53 -0300

    inclusão do reame

---
+++ js/dolorsit.js
@@ -0,0 +1,49 @@
+(function () {
+   //elemento.classList.remove('minhaClasse');
+//elemento.classList.add('minhaClasse');
+
+    var bodi = document.querySelector('body'); 
+
+    bodi.classList.remove('no-js')
+    bodi.classList.add('js')
+
+var menu = new Menu({
+container: '.header__nav',
+toggleBtn: '.header__btnMenu',
+widthEnable: 1024
+})
+
+var carouselImgs = new Carousel({ 
+
+container: 'laptop-slider .slideshow',
+itens: 'figure',
+btnPrev: '.prev',
+btnNext: '.next'
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
+
+
+
+
+
